7e94f0680886ba96ace6b7b9ad60593e7e0bf7ac
Juho Simojoki <juho.simojoki@outlook.com>
2025-01-21 13:05:11 +0200

    Error component is now functional

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,5 +1,28 @@
-const Error = (errorMsg, setError) => {
-  return null
+import { useEffect } from "react"
+
+const Error = ({ errorMsg, setError }) => {
+  useEffect(() => {
+    if(errorMsg) {
+      setTimeout(() => {
+        setError(null)
+      }, 5000)
+    }
+  }, [errorMsg])
+  
+  if(!errorMsg) {
+    return null
+  }
+
+  const errorStyle = {
+    color: 'red',
+    border: '2px solid red'
+  }
+
+  return(
+    <div style={errorStyle}>
+      {errorMsg}
+    </div>
+  )
 }
 
 export default Error
